709bab3f0aa6cd72efee15acdd781b0ace15a9c6
Gaurav <146842271+Gaurav-0211@users.noreply.github.com>
2024-12-17 21:50:51 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,85 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./models/user');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB Atlas
+const mongoURI = 'mongodb+srv://abhay62716:XOLhv5EFoCYIInBq@cluster0.os931.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+// CREATE a user (POST)
+app.post('/users', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// READ all users (GET)
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// READ a user by ID (GET)
+app.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// UPDATE a user by ID (PUT)
+app.put('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// DELETE a user by ID (DELETE)
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    res.status(200).send({ message: 'User deleted' });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ user.js
@@ -0,0 +1,19 @@
+// models/user.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  //email: {
+ //  type: String,
+ // },
+  age: {
+    type: Number,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
